6d24f0b288dfb672e5a74abbe1bcb3f6ef5a90c5
Janice Darling <janicedarling1@gmail.com>
2018-04-14 11:57:30 -0400

    Generating labels, comments & authors

--- utils.py
+++ utils.py
@@ -1,20 +1,18 @@
 import bz2
-import re
 
 
 file = "/Volumes/RedditData/train-unbalanced.csv.bz2"
 
-with bz2.BZ2File(file, "r") as bzfin:
-    lines = []
-    for i, line in enumerate(bzfin):
-        if i == 1:
-            break
-        print(line)
-
-
-
-
-
+# Read label, comment and author from file
+def getData(file):
+    with bz2.BZ2File(file, "r") as bzfin:
+        lines = []
+        for i, line in enumerate(bzfin):
+            # Yield 2000 lines of data
+            if i == 2000:
+                break
+            label, comment, author = line.decode('utf-8').split('\t')[0:3]
+            yield label, comment, author
 
 
 
